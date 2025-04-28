1d0e901fdbb8ca404f43aeb6c5f65d6cec4fc7ac
Winaldi <2381030@unai.edu>
2025-04-28 07:35:06 +0700

    ubah background formnya

--- src/components/CategoryForm.tsx
+++ src/components/CategoryForm.tsx
@@ -86,7 +86,7 @@ const CategoryForm = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 bg-slate-100 bg-opacity-30 flex items-center justify-center z-50 p-4">
       <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg overflow-hidden w-full max-w-md">
         <div className="flex justify-between items-center p-4 border-b dark:border-gray-700">
           <h2 className="text-lg font-medium text-gray-800 dark:text-gray-200">
